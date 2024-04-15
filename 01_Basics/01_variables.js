a733a3919c885ea1ca5dba1a62b93c66ea64c4dd
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-04-15 20:38:46 +0000

    variables_In_Js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID=1244453;
+let accountEmail="ibrarhassan@00outlook.com";
+let accountState;
+var accountPassward="12255";
+accountCity="Lahore"
+
+console.log(accountID);
+
+accountEmail="ibrarhassanansari@gmail.com";
+accountPassward="55882";
+accountCity="Karachi";
+
+console.table([accountID,accountEmail,accountState,accountPassward,accountCity]);
