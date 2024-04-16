63a916d17f42b6d2010e5994f50ec7125a97606c
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-04-16 20:53:33 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not broswer
+
+let name ="ibrar" // data type string they hold texal data
+
+let age = 18 // data type number they hold numeric value
+
+let isLoggedIn = false //data type boolean they hold ture & false
+
+let state; // data type undefine they assign nating value
+
+let temperature = null // data type object they hold null
+
+let data = BigInt("123") // data type bigint they numeric value & print value with n
+
+let data1 = Symbol(false) // data type symbol they hold any any type of value with uniqe identity
+
+let userData={
+    userName:"ibrar hassan",
+    contact : 3402324873,
+    youTubeChannel: true,
+}
+userData.contact=3170243582
+userData["userName"]="Ali hassan"
+
+console.log(userData.contact);
+console.log(userData["userName"]);
+
+// all code are working 
+
+// Types Any & Type Unknow typescript ky data types hain.
+
+
+
