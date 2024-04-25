ba384d6039faa53a6a3f023266145f32e2189754
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-04-25 08:34:27 +0000

    03_conversionOperation.js

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,60 @@
+// let score = 33;
+
+// console.log(score); // 33
+// console.log(typeof score); // number 
+// console.log(typeof(score)); // number
+
+//-------------------------------------------number In To String-------------------------------------------------------------
+// let valueINString_score=String(score);
+
+// console.log(valueINString_score); // 33
+// console.log(typeof valueINString_score); // string
+// console.log(typeof(valueINString_score)); // string
+
+//-------------------------------------------string In To Number-----------------------------------------------------------
+// let valueInNumber_score=Number(score);
+
+// console.log(valueInNumber_score); // 33
+// console.log(typeof valueInNumber_score); // number
+// console.log(typeof(valueInNumber_score)); // number
+
+//-----------------------------------------number+Sting Data In To Number---------------------------------------------------
+// let score1="123abc"
+
+// console.log(score1); // 123abc
+// console.log(typeof score1) // string
+
+// let valueINnumber_score1=Number(score1);
+
+// console.log(valueINnumber_score1); // NaN
+// console.log(typeof valueINnumber_score1); // number
+
+//---------------------------------------------string In To Boolen-----------------------------------------------------
+// let score3 = "";
+// let score4 = "ibrar"
+// // let valueInBoolean_sore3sore4=Boolean( score3 + score4);
+
+// let valueInBoolean_sore3=Boolean(score3);
+// let valueInBoolean_sore4=Boolean(score4);
+
+// console.log(valueInBoolean_sore3) // false
+// console.log(valueInBoolean_sore4);// ture
+
+// console.log (typeof valueInBoolean_sore3); // boolean
+// console.log (typeof valueInBoolean_sore4); // boolean
+
+//--------------------------------------------number In To Boolean--------------------------------------------------------------
+// let score5 = 0;
+// let score6 = "1"
+
+// // let valueInBoolean_sore5sore6=Boolean( score3 + score4);
+
+// let valueInBoolean_sore5=Boolean(score5);
+// let valueInBoolean_sore6=Boolean(score6);
+
+// console.log(valueInBoolean_sore5) // false
+// console.log(valueInBoolean_sore6);// ture
+
+// console.log (typeof valueInBoolean_sore5); // boolean
+// console.log (typeof valueInBoolean_sore6); // boolean
+
