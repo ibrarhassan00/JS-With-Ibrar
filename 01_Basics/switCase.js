ba532d378b22da71a70ecad6ec8f9cd51e852bd6
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-04-26 16:38:34 +0000

    switchCaseDayOff

---
+++ 01_Basics/switCase.js
@@ -0,0 +1,16 @@
+let dayOff = "1";
+
+switch(dayOff){
+
+case "1" : console.log("Monday");
+break;
+case "2" : console.log("Tuesday");
+break;
+case "3" : console.log("Wednesday");
+break;
+case "4" : console.log("Thursday");
+break;
+
+default: console.log("Please Enter Right Number");
+
+}
