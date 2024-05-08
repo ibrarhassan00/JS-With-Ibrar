595f039f06d00419274916e7980e4324e5c045e6
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-05-08 16:52:27 +0000

    Number in math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400
+// console.log(score); // 400
+
+const balance = new Number(100)
+// console.log(balance); // [Number: 100]
+
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(1)); // 100.0
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3)); // 124 . jo value number hum aurgument main pass karengy utna hi digit y print karega or last wale ko round kardeta hai . use for only decimal
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
