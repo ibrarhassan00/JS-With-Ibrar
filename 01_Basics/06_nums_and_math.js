7bb762a93081511d5f84c2900e80aa83ade21bf5
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-05-10 19:09:51 +0000

    Math Function

--- 01_Basics/06_nums_and_math.js
+++ 01_Basics/06_nums_and_math.js
@@ -15,3 +15,25 @@ const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+
+console.log(Math.abs(-4)); // 4
+console.log(Math.ceil(5.1)); // 6
+console.log(Math.round(5.6)); // 6 
+console.log(Math.round(5.4)); // 5
+console.log(Math.floor(4.9)); // 4
+
+
+console.log(Math.min(4, 3, 6, 8)); // 3
+console.log(Math.max(4, 3, 6, 8)); //  8
+
+console.log((Math.random()*10)+1)
+
+console.log(Math.random()); // 0 to .9
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
