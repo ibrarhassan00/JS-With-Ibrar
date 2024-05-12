a4cea3e818557103c8be33799e5c2d21a89ee2a7
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-05-12 07:50:59 +0000

    date

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,45 @@
+// Dates today date is 12-05-2024
+
+let myDate = new Date() // for current date 
+// console.log(myDate.getDate());             // 12
+// console.log(myDate.getDay());              // 0  week sunday sy start hota hai isliye sunday ko 0 
+// console.log(myDate.getFullYear());         //2024
+// console.log(myDate.getHours());            // Greenwish Mean Time GMT ( 7 ) and Pakistan Standard time PKT 12 
+// console.log(myDate.getMilliseconds());     // 712
+// console.log(myDate.getMonth());            // 4    q ky januanry 0 hota hai
+// console.log(myDate.getSeconds());          // 45
+// console.log(myDate.getTime());             // 1st january 1970 sy abtak y current time tak ka secound.
+console.log(myDate.getTimezoneOffset());
+// console.log(myDate.getUTCDate());
+// console.log(myDate.getUTCDay());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+
+
+
+
+
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
+    
+// })
