a4290efbe50a4356783fc6f587cb1da19d408978
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-07-07 04:19:43 +0000

    update

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,6 +1,10 @@
+var x; // Declaration
+x = 10; // Initialization
+var y = 20; // Declaration and initialization in one step
+
 const accountID=1244453;
 let accountEmail="ibrarhassan@00outlook.com";
-let accountState;
+let accountState;              // Declaration   
 var accountPassward="12255";
 accountCity="Lahore"
 
