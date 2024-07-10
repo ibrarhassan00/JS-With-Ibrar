4f1e0a0723be748669a0dcd203a27fa11f61ba22
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-07-10 11:14:33 +0000

    update

--- 01_Basics/05_operators.js
+++ 01_Basics/05_operators.js
@@ -1,3 +1,4 @@
+
 //-----------------------------------arithmeticOperators----------------------------------------
 
 let a = 5;
@@ -34,4 +35,84 @@ console.log("a-=4 =" , a-=4 ); // 1
 console.log("a*=4 =" , a*=4 ); // 20
 console.log("a+/4 =" , a/=4 ); // 1.25
 console.log("a+%4 =" , a%=4 ); // 1
+
+ // in sub ko sath print nahi karna warna y loop ki tarha calculate hongy.
+
+ //--------------------------logicalOperators-------------------------------------------
+
+// AND &&
+// OR  ||
+// NOT !
+ 
+
+// cond1  cond2   &&  ||
+//  T       T      T   T
+//  F       T      F   T
+//  T       F      F   T
+//  F       F      F   F
+
+let d = 6;
+let e = 5;
+
+let cond1 = d>e; // true
+let cond2 = d===e; // false
+/*
+console.log(cond1 && cond2) // false
+console.log(cond1 || cond2) // false
+console.log("d===6", d===6) // true
+console.log("!(d<e)",!(d<e)); // true
+*/
+
+//-------------------------------------unaryOperators-------------------------------------
+
+let f =5;
+
+//Preincrement
+ 
+console.log("f =", ++f); //6
+console.log("f =", f); //6
+
+//preDecrepment 
+
+console.log("f =", --f); //4
+console.log("f =", f); //4
+
+//postIncrement 
+
+console.log("f =", f++); //5
+console.log("f =", f); //6
+
+//postDecrement 
+
+console.log("f =", f--); //5
+console.log("f =", f); //4
+
+//-----------------------------comparisionOperators---------------------------------------------
+
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 <= 1); // false
+// console.log(2 == 1); // false
+// console.log("1" == 1); // ture
+// console.log("1" === 1); // false  data type bhi check kia.
+
+// != NOT EQUAL TO 
+// !-- NOT EQUAL TO & TYPE
+
+// console.log(2 != 1); // true  // 2 baraber nahi hai 01 ky . or y prove horaha hai isliye "ture"
+//console.log("2" != 2) // false
+//console.log("2" !== 2) // ture
+
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+console.log("2" === 2);// false
+
 
