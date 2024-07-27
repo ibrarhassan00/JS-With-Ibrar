a732a4acc1623487527bd1a113af11f63d87e0c7
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-07-27 08:47:09 +0000

    Update

--- 01_Basics/08.js
+++ 01_Basics/08.js
@@ -56,45 +56,108 @@
 // let entries = Object.entries(user2);
 // console.log("Entries:", entries); ///[["name", "ibrar"], ["contact", 3402324873], ["email", "abc@gmail.com"]]
 
-//Dot Notation:
+//---------------------------------------Key & Value Add by Dot Notation---------------------------------------------------
 
 //This is the simplest and most common way to add a property to an object.
 
-// let user = { 
+// let user4 = { 
 //     name: "Ibrar",
 //     contactNo: 3470243582,
 //     email: "ibrar@gmail.com"
 // };
 
-// user.age = 30;  // Adding a new property
+// user4.age = 30;  // Adding a new property
 // console.log(user);
 
-// Bracket Notation:
+//---------------------------------Key & Value Add By-Bracket Notation:-------------------------------------
 
 // This is useful when the property name is dynamic or not a valid identifier.
 
-// user["address"] = "123 Main St";
-// console.log(user);
+// user4["address"] = "123 Main St";
+// console.log(user4);
 
-// Object.defineProperty:
-
+//--------------------------------------- Object.defineProperty:----Nichy define kia hai-------------------------------------
+//Agar tum user object main ek property add karna chahte ho aur usay zyada control ke sath set karna chahte ho, toh tum Object.defineProperty method ka use kar sakte ho. Is tarah tum specify kar sakte ho ke wo property writable (change ho sakti hai ya nahi), enumerable (loops main show hogi ya nahi), aur configurable (delete ho sakti hai ya nahi) hai ya nahi.
 // This method allows more control over the property being added, such as setting it to be read-only or not enumerable.
 
-// Object.defineProperty(user, 'country', {
-//     value: 'USA',
-//     writable: true,  // Can be changed
-//     enumerable: true,  // Will show up in loops
-//     configurable: true  // Can be deleted or changed
-// });
-// console.log(user);
+// Pehle user object define karte hain
+let user5 = {};
 
-// Spread Operator:
+// Ab country property add karte hain `Object.defineProperty` ke sath
+Object.defineProperty(user5, 'country', {
+    value: 'USA',         // Property ka value
+    writable: true,       // Ye value change ho sakti hai
+    enumerable: true,     // Ye property loops main nazar ayegi
+    configurable: true    // Ye property delete ya change ho sakti hai
+});
 
-// This method creates a new object with the added properties, useful for creating immutable updates.
-// user = { ...user, hobby: "Reading" };
-// console.log(user);
+console.log(user5);
+//y out out hai--------{ country: 'USA' }
+// Is example main, humne user object main country property add ki aur usay 'USA' set kiya. Humne specify kiya ke:
 
-// Object.setPrototypeOf:
+// writable: true matlab country property ka value change kiya ja sakta hai.
+// enumerable: true matlab ye property loops (jaise for...in) main show hogi.
+// configurable: true matlab ye property delete ya change kiya ja sakta hai.
+//---------------------------------------------------------------------------------------------------
+//Yani user object main country property add ho gayi hai aur uska value 'USA' set ho gaya hai. Ab tum is property ko access kar sakte ho:
+
+console.log(user5.country); // Output: 'USA'
+
+// Agar tum user.country ka value change karna chaho:
+
+user5.country = 'Canada';
+console.log(user5.country); // Output: 'Canada'
+
+//Aur agar tumhe property ko delete karna ho:
+
+delete user5.country;
+console.log(user5); // Output: {}
+
+//------------------------------------Spread Operator:------------------------------------------------
+
+//This method creates a new object with the added properties, useful for creating immutable updates.
+//Spread operator (...) ka use karte huay hum ek naya object bana sakte hain jo existing object ki sari properties ko include kare aur kuch nayi properties bhi add kare. Ye tarika immutable updates ke liye bahut useful hai, matlab existing object ko change kiye bina naya object banana.
+
+// Pehle user object define karte hain
+let user6 = {
+    name: 'Ali',
+    age: 25
+};
+
+// Ab hum user object main hobby property add karte hain spread operator ka use kar ke
+user6 = { ...user6, hobby: 'Reading' };
+
+console.log(user6);
+
+//Is example main humne pehle user object define kiya jisme name aur age properties hain. Phir humne spread operator ka use kiya ...user jo ke existing user object ki sari properties ko naya object main copy kar deta hai aur saath hi hobby property add kiya.
+
+// ----y out put hia 
+// {
+//     name: 'Ali',
+//     age: 25,
+//     hobby: 'Reading'
+// }
+
+// Ab tumhare user object main hobby property bhi add ho gayi hai bina existing user object ko modify kiye.
+
+// Agar tum koi dusri property bhi add karna chaho ya existing property ko update karna chaho:
+
+
+// name property ko update karte hain aur city property add karte hain
+user6 = { ...user6, name: 'Ahmed', city: 'Lahore' };
+
+console.log(user6);
+
+// Ab y out put hoga
+// {
+//     name: 'Ahmed',
+//     age: 25,
+//     hobby: 'Reading',
+//     city: 'Lahore'
+// }
+
+
+//-----------------------------------------Object.setPrototypeOf:----------------------------------------
 
 // This method sets the prototype (i.e., the internal [[Prototype]] property) of a specified object to another object or null.
 // const additionalProperties = {
